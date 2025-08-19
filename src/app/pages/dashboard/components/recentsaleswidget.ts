51666486ab2e6d505da02877b483a63c8b4ed213
guilhermeroncon <guilhermeroncs@gmail.com>
2025-08-19 00:33:15 -0300

    Apresentar tcc
    sabao

--- src/app/pages/dashboard/components/recentsaleswidget.ts
+++ src/app/pages/dashboard/components/recentsaleswidget.ts
@@ -1,47 +1,65 @@
-import { Component } from '@angular/core';
-import { RippleModule } from 'primeng/ripple';
-import { TableModule } from 'primeng/table';
-import { ButtonModule } from 'primeng/button';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product, ProductService } from '../../service/product.service';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
 
 @Component({
-    standalone: true,
     selector: 'app-recent-sales-widget',
-    imports: [CommonModule, TableModule, ButtonModule, RippleModule],
-    template: `<div class="card !mb-8">
-        <div class="font-semibold text-xl mb-4">Recent Sales</div>
-        <p-table [value]="products" [paginator]="true" [rows]="5" responsiveLayout="scroll">
-            <ng-template #header>
-                <tr>
-                    <th>Image</th>
-                    <th pSortableColumn="name">Name <p-sortIcon field="name"></p-sortIcon></th>
-                    <th pSortableColumn="price">Price <p-sortIcon field="price"></p-sortIcon></th>
-                    <th>View</th>
-                </tr>
-            </ng-template>
-            <ng-template #body let-product>
-                <tr>
-                    <td style="width: 15%; min-width: 5rem;">
-                        <img src="https://primefaces.org/cdn/primevue/images/product/{{ product.image }}" class="shadow-lg" alt="{{ product.name }}" width="50" />
-                    </td>
-                    <td style="width: 35%; min-width: 7rem;">{{ product.name }}</td>
-                    <td style="width: 35%; min-width: 8rem;">{{ product.price | currency: 'USD' }}</td>
-                    <td style="width: 15%;">
-                        <button pButton pRipple type="button" icon="pi pi-search" class="p-button p-component p-button-text p-button-icon-only"></button>
-                    </td>
-                </tr>
-            </ng-template>
-        </p-table>
-    </div>`,
+    standalone: true,
+    template: `
+        <div class="card">
+            <div class="flex justify-content-between align-items-center mb-5">
+                <h5>Experimentos Recentes</h5>
+                <div>
+                    <button pButton label="Ver Todos" icon="pi pi-arrow-right" class="p-button-text"></button>
+                </div>
+            </div>
+            <p-table [value]="products" [rows]="5" [paginator]="true" responsiveLayout="scroll">
+                <ng-template pTemplate="header">
+                    <tr>
+                        <th style="width: 45%">Nome do Experimento</th>
+                        <th>Participantes</th>
+                        <th>Status</th>
+                        <th>Emoção</th>
+                        <th>Ações</th>
+                    </tr>
+                </ng-template>
+                <ng-template pTemplate="body" let-product>
+                    <tr>
+                        <td>{{ product.name }}</td>
+                        <td>{{ product.price }}</td> 
+                        <td>
+                            <p-tag [value]="product.inventoryStatus" [severity]="getSeverity(product.inventoryStatus)"></p-tag>
+                        </td>
+                        <td>{{ product.category }}</td>
+                        <td style="width: 15%">
+                            <button pButton pRipple icon="pi pi-search" class="p-button-rounded p-button-text"></button>
+                        </td>
+                    </tr>
+                </ng-template>
+            </p-table>
+        </div>
+    `,
+    imports: [CommonModule, TableModule, ButtonModule, TagModule],
     providers: [ProductService]
 })
-export class RecentSalesWidget {
+export class RecentSalesWidget implements OnInit {
     products!: Product[];
 
     constructor(private productService: ProductService) {}
 
     ngOnInit() {
         this.productService.getProductsSmall().then((data) => (this.products = data));
+    }
+
+    getSeverity(status: string) {
+        switch (status) {
+            case 'CONCLUÍDO': return 'success';
+            case 'EM ANDAMENTO': return 'warning';
+            case 'PLANEJADO': return 'info';
+            default: return 'secondary';
+        }
     }
 }
